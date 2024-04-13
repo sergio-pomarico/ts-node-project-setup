5982d0beeb88fbdcbd6405ba5502a9946434635b
Sergio Pomárico <sergiodavid21@gmail.com>
2024-04-13 12:05:45 -0500

    fix: :bug: fix boken test for CI/CD workflow

--- src/config/env.test.ts
+++ src/config/env.test.ts
@@ -2,10 +2,9 @@ import { env } from './env';
 
 describe('test enviroment vars', () => {
   test('should return test env', () => {
-    expect(env.db.host).toBe('ep-super-cloud-a5g34pau.us-east-2.aws.neon.tech');
+    expect(env.db.host).toBe('localhost');
     expect(env.db.port).toBe(5432);
-    expect(env.db.username).toBe('testing-database_owner');
-    expect(env.db.name).toBe('testing-database');
+    expect(env.server.port).toBe(3000);
   });
   test('should return a error if env vars are not set', async () => {
     jest.resetModules();
